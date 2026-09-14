de5b22f0e121ad99149ed6b248128fa6a62bd452
Maxim Milyavsky <mmilyavsky@gmail.com>
2026-09-14 07:27:03 +0300

    Compile Health Connect diagnostic against Android 36

--- android-healthconnect/app/build.gradle.kts
+++ android-healthconnect/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.maxim.healthconnectsleep"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.maxim.healthconnectsleep"
